3d45c70bd01a5334915ec6ab41052717892f98ff
lgriffithl <lliill.education.lliill@gmail.com>
2023-08-05 02:51:31 +0100

    fixes

--- src/index.js
+++ src/index.js
@@ -5,10 +5,11 @@ import { BrowserRouter } from 'react-router-dom';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <BrowserRouter>    
-      <App />
-    </BrowserRouter>
-  </React.StrictMode>
+  // <React.StrictMode>
+  //   <BrowserRouter>    
+  //     <App />
+  //   </BrowserRouter>
+  // </React.StrictMode>
+  <h1 style={{color:"white",fontSize:"40px",backgroundColor:"red"}}>It works </h1>
 );
 
